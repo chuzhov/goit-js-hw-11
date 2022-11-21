7ccc16b686b69c23f3f8f607babab014b573eb03
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2022-11-21 13:08:17 +0200

    Fixes simple lightbox issue

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './css/styles.css';
-//import debounce from 'lodash.debounce';
 import toastr from "toastr";
 import './css/toastr.css';
-import axios from "axios";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import axios from 'axios';
+import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+let lightbox = new SimpleLightbox('.gallery a', { captions: true, captionsData: "alt", captionDelay: 250 });
 
 const refs = {
     submitQuery : document.querySelector("#search-form"),
@@ -12,18 +13,23 @@ const refs = {
     loadMoreBtn : document.querySelector("button.load-more"),
 }
 
-const PER_PAGE = 40;
+const addNewPicturesGear = {
+    types: ["loadMoreBtn", "endlessScroll"],
+    current: "loadMoreBtn",
+}
+
+console.log(addNewPicturesGear.current);
+
 let currentUserQuery = "";
 
+const PER_PAGE = 40;
 let imagesQuantity = 0;
 let pagesQuantity = 0;
-let currenPageNum = 1
+let currentPageNum = 1
 
 refs.loadMoreBtn.classList.add("is-hidden");
 refs.submitQuery.addEventListener("submit", submitQueryHandler);
 refs.loadMoreBtn.addEventListener("click", loadMoreImages);
-
-const lightbox = new SimpleLightbox('.gallery a', { captions: true });
 
 async function submitQueryHandler(event) {
     event.preventDefault();
@@ -37,10 +43,16 @@ async function submitQueryHandler(event) {
     refs.loadMoreBtn.classList.add("is-hidden");
 
     currentUserQuery = userQuery;
-    
+    currentPageNum = 1;
+    // try {
+    //     const data = await axiosFetchPictures(currentUserQuery);
+    //     console.log("data ", data);
+    // }
+
+
     // try {
     //     const {results} = await axiosFetchPictures(currentUserQuery);
-
+    //     console.log(results);
     //     if (results.totalHits === 0) {
     //         toastr.error("Your fantasy has no limits! </br> Sorry, there are no images matching your search query");
     //         return;
@@ -48,11 +60,12 @@ async function submitQueryHandler(event) {
         
     //     renderGalleryItems( refs.imgGallery, getGalleryItems(results) );
 
-    // } catch (err) {
+    // } catch (error) {
     //     toastr.error("Run-time error: ", error);
     // } 
 
-    axiosFetchPictures(currentUserQuery).then(results => {
+    axiosFetchPictures(currentUserQuery)
+    .then(results => {
         
         if (results.totalHits === 0) {
             toastr.error("Your fantasy has no limits! </br> Sorry, there are no images matching your search query");
@@ -62,13 +75,12 @@ async function submitQueryHandler(event) {
         imagesQuantity = results.totalHits;
         pagesQuantity = Math.ceil( imagesQuantity / PER_PAGE );
 
-        toastr.success(`We found ${imagesQuantity} images. ${pagesQuantity} pages are allowed.`)
-                
+        toastr.success(`Great choice! We found ${imagesQuantity} images.`);
+
         renderGalleryItems( refs.imgGallery, getGalleryItems(results) );
         
-        lightbox.on('show.simplelightbox');
-
-    });
+    }).
+    catch((error)=> toastr.error(`Something went wrong: `, error));
     
 }
 
@@ -82,7 +94,7 @@ function getGalleryItems({hits}) {
             </a>
                 <div class="info">
                     <p class="info-item">
-                    <img src="./src/img/ico-views-bw.png" width="20" height="20" alt=""/>${item?.likes ?? 0}
+                    <img src="img/ico-views-bw.png" width="20" height="20" alt=""/>${item?.likes ?? 0}
                     </p>
                     <p class="info-item">
                     <b>Views
@@ -106,48 +118,48 @@ function getGalleryItems({hits}) {
 }
 
 function renderGalleryItems(element, HTMLstring) {
-    element.insertAdjacentHTML("beforeend", HTMLstring);
     
-    if (currenPageNum * PER_PAGE < imagesQuantity) {
+    element.insertAdjacentHTML("beforeend", HTMLstring);
+    lightbox.refresh();
+    
+    if (currentPageNum * PER_PAGE < imagesQuantity) {
         refs.loadMoreBtn.classList.remove("is-hidden");
     } else refs.loadMoreBtn.classList.add("is-hidden");
 }
 
-async function axiosFetchPictures(q) {
-
-const hostURL = 'https://pixabay.com/api/';
-//axios.defaults.headers.common['Authorization'] = '31408282-c2e36d8dbb4ab0017da4fad76';
-const API_KEY = '31408282-c2e36d8dbb4ab0017da4fad76';
-
- const searchParams = {
-    q,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    page: currenPageNum,
-    per_page: PER_PAGE,
-};
-
-const url = `${hostURL}?key=${API_KEY}&q=${searchParams.q}&page=${currenPageNum}&per_page=${searchParams.per_page}`
-    
-    try {    
-       // const response = await axios.get(`${baseURL}?${searchParams}`);
-
-       const { data } = await axios.get(url);
-       return data;
-
-    } catch (err) {
-        console.log(err);
-    } 
-}
-
 function loadMoreImages() {
-
-    currenPageNum+=1;
+   
+    if (currentPageNum <= pagesQuantity) currentPageNum+=1
+    else return;
 
     axiosFetchPictures(currentUserQuery).then(results => {
         renderGalleryItems( refs.imgGallery, getGalleryItems(results) );
     });
-    console.log(lightbox);
-    lightbox.refresh();
+    
 }
+
+async function axiosFetchPictures(q) {
+
+    const hostURL = 'https://pixabay.com/api/';
+    //axios.defaults.headers.common['Authorization'] = '31408282-c2e36d8dbb4ab0017da4fad76';
+    const API_KEY = '31408282-c2e36d8dbb4ab0017da4fad76';
+    
+     const searchParams = {
+        q,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: currentPageNum,
+        per_page: PER_PAGE,
+    };
+    
+    const url = `${hostURL}?key=${API_KEY}&q=${searchParams.q}&page=${currentPageNum}&per_page=${searchParams.per_page}`
+
+        try {    
+           const { data } = await axios.get(url);
+           return data;
+    
+        } catch (err) {
+            console.log(err);
+        } 
+    }
